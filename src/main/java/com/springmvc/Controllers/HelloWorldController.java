9b3c5f75a8bfcece16e2eb3264a667d1d6714b83
vinokurovaa <CSLAmoscow2111>
2017-06-16 10:49:31 +0300

    add SimpleController and jsp

---
+++ src/main/java/com/springmvc/Controllers/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.springmvc.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloWorldController {
+    @RequestMapping(value={ "/hello"}, method = RequestMethod.GET)
+    public String printHello(ModelMap model) {
+        model.addAttribute("message", "Hello Spring!");
+        return "hello";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/hello.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: anvi0616
+  Date: 6/16/2017
+  Time: 10:28 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType = "text/html; charset = UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<html>
+<head>
+    <title>Hello World</title>
+</head>
+
+<body>
+<h2>${message}</h2>
+</body>
+</html>
