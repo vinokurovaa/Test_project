7ab43a1519ff8b30dec15baa1ad6cf7626afe083
vinokurovaa <CSLAmoscow2111>
2017-06-21 14:44:08 +0300

    add signup

--- src/main/java/com/springmvc/Controllers/Test.java
+++ src/main/java/com/springmvc/Controllers/Test.java
@@ -1,16 +1,27 @@
 package com.springmvc.Controllers;
 
+import com.springmvc.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Locale;
+import java.util.Map;
 
 @Controller
 public class Test {
     @RequestMapping(value = {"/test"}, method = RequestMethod.GET)
-    public ModelAndView test(Locale locale){
-        return new ModelAndView("test");
+    public ModelAndView regis(Locale locale, Map<String, Object> mv) {
+        User user = new User();
+        mv.put("newUser", user);
+        return new ModelAndView("test", mv);
+    }
+
+    @RequestMapping(value = {"/test"}, method = RequestMethod.POST, params = {"email","firstName", "lastName"})
+    public ModelAndView setPassword(@ModelAttribute("newUser") User newUser, Locale locate, BindingResult bindingResult) {
+        return new ModelAndView("test1");
     }
 }
